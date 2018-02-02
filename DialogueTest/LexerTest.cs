13c1ffa9f7c80e9b14038b79f2fe0a5d0d887bec
RangHo Lee <hibiki_love@rangho.moe>
2018-02-02 14:37:45 -0800

    Update unit test project
    Now it uses Visual Studio test adapter, but I will make it NUnit-compatible later too.

--- DialogueTest/LexerTest.cs
+++ DialogueTest/LexerTest.cs
@@ -1,67 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using NUnit.Framework;
-
-using RangHo.DialogueScript;
-using RangHo.DialogueScript.Token;
-using RangHo.DialogueScript.Utility;
-
-namespace RangHo.DialogueScript.Test
+namespace DialogueTest
 {
-    [TestFixture]
+    [TestClass]
     public class LexerTest
-    {        
-        [Test]
-        public void TestSayStatement()
-        {
-            string testScript = "character : \"Hello, world!\"";
-            Lexer lexer = new Lexer(testScript);
-            Console.WriteLine("Lexer is initialized.");
+    {
 
-            AbstractToken[] tokens = lexer.ReadAll();
-            Console.WriteLine("Finished reading tokens.");
-            AbstractToken[] expected =
-            {
-                new IdentifierToken("character"),
-                new PunctuationToken(":"),
-                new StringToken("Hello, world!")
-            };
-
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                Assert.That(tokens[i].Content, Is.EqualTo(expected[i].Content));
-                Console.WriteLine(tokens[i]);
-            }
-        }
-
-        [Test]
-        public void TestComment()
-        {
-            string testScript = "# this is a comment \n"
-                                + "character: \"Hello, world!\"";
-            Lexer lexer = new Lexer(testScript);
-            Console.WriteLine("Lexer is initialized.");
-
-            AbstractToken[] tokens = lexer.ReadAll();
-            Console.WriteLine("Finished reading tokens.");
-            AbstractToken[] expected =
-            {
-                new NewLineToken(),
-                new IdentifierToken("character"),
-                new PunctuationToken(":"),
-                new StringToken("Hello, world!")
-            };
-
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                Assert.That(tokens[i].Content,
-                       Is.EqualTo(expected[i].Content));
-                Console.WriteLine(tokens[i]);
-            }
-        }
     }
 }
